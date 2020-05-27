25efbe31a9db46e96491f45552ec3b64d6cae9c8
121naim <56754920+121naim@users.noreply.github.com>
2020-05-27 22:58:22 +0100

    Update randomly select rows.py

--- randomly select rows.py
+++ randomly select rows.py
@@ -1,5 +1,5 @@
 import pandas as pd 
 import numpy as np 
 
-df = pd.read_excel('C:/Users/Naim/Documents/sample 05-02-2020.xlsx')
+df = pd.read_excel('sample05-02-2020.xlsx')
 df_1 = df.sample( frac = 0.50)
