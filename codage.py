d72944f85d1afab2ecf05fdcd9349f4e9121831c
121naim <56754920+121naim@users.noreply.github.com>
2020-05-27 22:52:54 +0100

    Update codage.py

--- codage.py
+++ codage.py
@@ -14,7 +14,7 @@ from collections import namedtuple
 import warnings
 warnings.filterwarnings('ignore')
 survey_data = pd.read_excel('CJD.xlsx' , sep=';', error_bad_lines=False, index_col=False, dtype='unicode' , encoding = 'UTF-8' )
-ccc = pd.read_excel('C:/Users/user/.spyder-py3/CJD/ccc1.xlsx' , sep=';', error_bad_lines=False, index_col=False, dtype='unicode' , encoding = 'UTF-8' )
+ccc = pd.read_excel('ccc1.xlsx' , sep=';', error_bad_lines=False, index_col=False, dtype='unicode' , encoding = 'UTF-8' )
 #ccc = ccc.drop_duplicates(['Q8Other1', 'Q8Other2','Q8Other3', 'Q8Other4'])
 
 
@@ -62,11 +62,11 @@ dfDiff = pd.DataFrame(columns=colNames,data=fillerData)
 for i in range(len(uniqueValsList)):
     dfDiff[colNames[i]] = uniqueValsList[i]
        
-dfDiff.to_excel('C:/Users/user/.spyder-py3/CJD/dfDiff.xlsx', index = True)
+dfDiff.to_excel('dfDiff.xlsx', index = True)
 
 frames = [ccc,dfDiff,df1]
 result = pd.concat(frames, axis = 1 )  
-result.to_excel('C:/Users/user/.spyder-py3/CJD/result.xlsx', index = True)
+result.to_excel('result.xlsx', index = True)
 
 """new = df.columns.tolist()
 length = len(new)
@@ -88,7 +88,7 @@ i, r = pd.factorize(Q8_other_cd.Unique_Values)
 choices = np.arange(max(124, r.size))
 c = np.random.choice(choices, r.shape, False)
 Q8_other_cd['c'] = Q8_other_cd.assign(Unique_Values=c[i])
-Q8_other_cd.to_excel('C:/Users/user/.spyder-py3/CJD/Q8_other_cd.xlsx') """
+Q8_other_cd.to_excel('Q8_other_cd.xlsx') """
 
 
 
@@ -99,6 +99,6 @@ Q8_other_cd.to_excel('C:/Users/user/.spyder-py3/CJD/Q8_other_cd.xlsx') """
     .set_axis([ 'Unique_Values'], axis=1, inplace=False
     """
 #comments : #########
-#df = pd.read_csv("C:/Users/user/.spyder-py3/pg30_marroc/pg.csv", sep=';', error_bad_lines=False, index_col=False, dtype='unicode' , encoding = 'UTF-8')
+#df = pd.read_csv("pg.csv", sep=';', error_bad_lines=False, index_col=False, dtype='unicode' , encoding = 'UTF-8')
 #Q8_other_c = (survey_data['Q8Other1'].append( survey_data['Q8Other2'])).unique()
 #Q8_other_c = pd.DataFrame(Q8_other_c)     #############
